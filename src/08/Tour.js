4c9b6c295e38c4657e0303149b47882ab74f2e03
Icecaptin <tjdqn0228@gmail.com>
2023-05-16 11:49:44 +0900

    20230516_Loading중 추가

--- src/08/Tour.js
+++ src/08/Tour.js
@@ -1,19 +1,25 @@
 import React, { useState, useEffect, useRef } from 'react';
 import GalleryView from './GalleryView';
 import styles from './Tour.module.css';
+import loadingImg from './loading.gif';
+
 
 const Tour = () => {
     const txt1 = useRef();
     const [inputKey, setInputKey] = useState('');
     const [searchClicked, setSearchClicked] = useState(false);
+    const [loading, setLoading] = useState(false); // 로딩 상태 추가
 
     const handleInputChange = (e) => {
         setInputKey(e.target.value);
     };
 
-    const searchKeywords = (e) => {
+    const searchKeywords = async (e) => {
         e.preventDefault();
+        setLoading(true); // 로딩 상태 변경
         setSearchClicked(true);
+        await someAsyncFunction(); // 비동기 작업 예시 (실제로는 데이터를 가져오는 등의 작업을 수행)
+        setLoading(false); // 로딩 상태 변경
     };
 
     const searchClear = (e) => {
@@ -28,17 +34,27 @@ const Tour = () => {
         }
     };
 
+    const someAsyncFunction = () => {
+        return new Promise((resolve) => {
+            // 예시: 2초 후에 비동기 작업 완료
+            setTimeout(() => {
+                resolve();
+            }, 2000);
+        });
+    };
+
     return (
         <>
             <main>
                 <article>
                     <div className="grid">
-                        <input ref={txt1} type="text" id="txt1" name="txt1" placeholder="입력하세용" className={styles.inputtour} value={inputKey} onChange={handleInputChange} onKeyDown={handleKeyPress} required />
+                        <input ref={txt1} type="text" id="txt1" name="txt1" placeholder="입력하세용" className={styles.inputtour} value={inputKey} onChange={handleInputChange} onKeyPress={handleKeyPress} required />
                         <div className={styles.btns}>
-                            <button onClick={searchKeywords}>검색</button>
-                            <button onClick={searchClear}>취소</button>
+                            <button disabled={loading} onClick={searchKeywords}>검색</button>
+                            <button disabled={loading} onClick={searchClear}>취소</button>
                         </div>
                     </div>
+                    {loading && <div className={styles.loading}><img src={loadingImg} alt="로딩 중" />로딩 중...</div>} {/* 로딩 중 아이콘 표시 */}
                 </article>
                 <div>
                     {searchClicked && <GalleryView inputKey={inputKey} />}
